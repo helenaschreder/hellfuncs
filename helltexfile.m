ebab32610850e7e0d7792cd59ffaa0d8b579c77a
helenaschreder <114764895+helenaschreder@users.noreply.github.com>
2024-01-10 13:47:32 -0800

    made the headers hearts :) ♥

--- helltexfile.m
+++ helltexfile.m
@@ -1,4 +1,5 @@
 function helltexfile(parts, subparts)
+% helltexfile(parts, subparts)
 % function to make tex file for my homework
 % inputs:
 % parts: # problems in set
@@ -7,9 +8,24 @@ function helltexfile(parts, subparts)
 % example:
 % problem set with 1 (a,b,c); 2 (a,b,c,d,e,f); 3
 % fileout = helltexfile(3,[3,6,0])
+% 
+% possible updates:
+% change to make a whole file, not just a txt file
+% change to go to cd
+
+% allows user to input a cell with the names of parts. otherwise just
+% numbered
+if numel(parts) > 1
+    partnames = parts;
+    partnum = numel(parts);
+
+else
+    partnames = string(1:parts);
+    partnum = parts;
+end
 
 %check input
-if numel(subparts)~=parts
+if numel(subparts)~=partnum
     error('The subpart input vector must be the length of the number of parts requested')
 end
 
@@ -17,9 +33,10 @@ if sum(subparts>26)~=0
     error('You have more subparts than there are letters of the alphabet, did not plan for that')
 end
 
+
 % location to save file to
 destination = '/Users/helenaschreder/Desktop/';
-filename = 'helltext.txt';
+filename = 'helltext.tex';
 fname = fullfile(destination,filename);
 
 % letters to be printed
@@ -31,6 +48,7 @@ begin_darkbox = '\begin{darkbox}';
 end_darkbox = '\end{darkbox}';
 begin_lightbox = '\begin{lightbox}';
 end_lightbox = '\end{lightbox}';
+temptext = '\section*{Problem ';
 
 % blank
 blanktext = '...';
@@ -41,24 +59,38 @@ cw = 64;
 % so first entry overwrites
 first = 1;
 
-for i1 = 1:parts
+for i1 = 1:partnum
+
+    repnum = round((cw-10-numel(partnames{i1}))/2)-1;
+
 
     % main part
-    header1 = ['%' repmat(num2str(i1),[1,cw-2]) '%'];
-    header2 = ['%' repmat('-',[1,(cw-12)/2]) ' Problem ' num2str(i1) ' ' repmat('-',[1,(cw-12)/2-1]) '%'];
+    % header1 = ['%' repmat(num2str(i1),[1,cw-2]) '%'];
+    header1 = ['%' repmat('♥ ',[1,round(cw-2)/2]) '%'];
+    header2 = ['%' repmat('-',[1,repnum]) ' Problem ' partnames{i1} ' ' repmat('-',[1,repnum]) '%'];
     header3 = header1;
-    footer = header1;
-    f = sprintf('%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n',header1,header2,header3,begin_darkbox,blanktext,end_darkbox,footer);
+    % footer = header1;
+    footer = ['%' repmat('-',[1,(cw-2)]) '%'];
+    f = sprintf('%s\n%s\n%s\n%s%s}\n%s\n%s\n%s\n%s\n\n',header1,header2,header3,temptext,partnames{i1},begin_darkbox,blanktext,end_darkbox,footer);
     savetext(f,first,fname)
     first = 0;
 
     % sub  parts
     if subparts(i1)~=0
     for i2 = 1:subparts(i1)
-        header1 = ['%' repmat(num2str(i1),[1,cw-2]) '%'];
-        header2 = ['%' repmat('-',[1,(cw-12)/2-1]) ' Problem ' [num2str(i1) alphabet(i2)] ' ' repmat('-',[1,(cw-12)/2-1]) '%'];
-        header3 = ['%' repmat(ALPHABET(i2),[1,cw-2]) '%'];
-        footer = ['%' repmat([num2str(i1) ALPHABET(i2)],[1,(cw-2)/2]) '%'];
+
+        repnum = round((cw-12-numel(partnames{i1}))/2)-1;
+
+
+
+        header1 = ['%' repmat('♡ ',[1,round(cw-2)/2]) '%'];
+        % header1 = ['%' repmat([num2str(i1) ALPHABET(i2)],[1,(cw-2)/2]) '%'];
+        header3 = header1;
+        % header1 = ['%' repmat(num2str(i1),[1,cw-2]) '%'];
+        header2 = ['%' repmat('-',[1,repnum]) ' Problem ' [partnames{i1} ' ' alphabet(i2)] ' ' repmat('-',[1,repnum]) '%'];
+        % header3 = ['%' repmat(ALPHABET(i2),[1,cw-2]) '%'];
+        % footer = ['%' repmat([num2str(i1) ALPHABET(i2)],[1,(cw-2)/2]) '%'];
+        footer = ['%' repmat('-',[1,(cw-2)]) '%'];
         f = sprintf('%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n',header1,header2,header3,begin_lightbox,[alphabet(i2) '. '],end_lightbox,footer);  
         savetext(f,first,fname)
     end
